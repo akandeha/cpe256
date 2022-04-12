234d3e5084551e61a9575e75d1b637e2d880e789
habib <akandeha43@gmail.com>
2022-04-12 21:35:43 +0300

    Changed Main to CaesarCipher

--- src/com/cpe256/habib/CaesarCipher.java
+++ src/com/cpe256/habib/CaesarCipher.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class Main {
+public class CaesarCipher {
     public static void main(String[] args) throws FileNotFoundException {
 
         final char[] english = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz".toCharArray();
